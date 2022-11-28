791c443c3a52ab0bd388d321584756e09b72298a
sergejsvoronins <svoronins@gmail.com>
2022-11-28 14:44:46 +0100

    span event is added

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -38,6 +38,9 @@ function createHtml(someList: Task[]) {
     todoContainer.appendChild(todo);
     todo.appendChild(todoDesc);
     todo.appendChild(deleteBtn);
+    todoDesc.addEventListener("click", ()=>{
+        todoDesc.classList.toggle("done");
+    })
     deleteBtn.addEventListener("click", () => {
       deleteTask(todoList, i);
       getFromLS();
