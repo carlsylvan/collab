d0697ba5bae1e20ead448656608b5bb7fca8b951
ehnvik <gustav.ehnvik@gmail.com>
2022-11-28 13:26:19 +0100

    Added deleteTask function

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,7 +3,12 @@ function sendToLS(task: Task[]): void {
   localStorage.setItem("todolist", JSON.stringify(task));
 }
 
-let todoList : Task [] = [];
-function getFromLS(){
-    todoList = JSON.parse(localStorage.getItem("todolist") || "[]");
+let todoList: Task[] = [];
+function getFromLS() {
+  todoList = JSON.parse(localStorage.getItem("todolist") || "[]");
+}
+
+function deleteTask(taskList: Task[], index: number) {
+  taskList.splice(index, 1);
+  sendToLS(taskList);
 }
